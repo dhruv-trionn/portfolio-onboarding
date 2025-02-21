802dac1713bffce53188a92355c1a109fa1b8831
Dhruv Solanki <dhruv.trionn@gmail.com>
2025-02-21 23:09:38 +0530

    Task:2 Create custom posttype's Taxonomy with three default categories

--- wp-content/themes/hello-theme-child/inc/custom-post-types.php
+++ wp-content/themes/hello-theme-child/inc/custom-post-types.php
@@ -31,10 +31,43 @@ class registerPostType
                 'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'page-attributes'), // Enable title, editor, featured images etc.
                 'menu_position' => 5,
                 'menu_icon' => 'dashicons-portfolio',
-                'rewrite' => array('slug' => 'integrations', 'with_front' => false),
+                'rewrite' => array('slug' => 'portfolio', 'with_front' => false),
                 'show_ui' => true,
             )
         );
+
+        /* Portfolio Custom Taxonomy */
+        register_taxonomy('project_category', array('portfolio'), array(
+            'hierarchical' => true,
+            'labels' => array(
+                'name' => __('Project Categories', 'portfolio'),
+                'singular_name' => __('Project Category', 'portfolio'),
+                'search_items' => __('Search Project Categories', 'portfolio'),
+                'all_items' => __('All Project Categories', 'portfolio'),
+                'parent_item' => __('Parent Project Categories', 'portfolio'),
+                'parent_item_colon' => __('Parent Project Category:', 'portfolio'),
+                'edit_item' => __('Edit Project Category', 'portfolio'),
+                'update_item' => __('Update Project Category', 'portfolio'),
+                'add_new_item' => __('Add New Project Category', 'portfolio'),
+                'new_item_name' => __('New Project Category Name', 'portfolio'),
+                'menu_name' => __('Project Categories', 'portfolio'),
+            ),
+            'show_ui' => true,
+            'query_var' => true,
+            'rewrite' => array('slug' => 'project-category', 'with_front' => false),
+            'show_admin_column' => true
+        ));
+
+        // Add three categories when the taxonomy is registered
+        if (!term_exists('Web Developement', 'project_category')) {
+            wp_insert_term('Web Developement', 'project_category');
+        }
+        if (!term_exists('Next Js', 'project_category')) {
+            wp_insert_term('Next Js', 'project_category');
+        }
+        if (!term_exists('Wordpress', 'project_category')) {
+            wp_insert_term('Wordpress', 'project_category');
+        }
 
     }
 }
